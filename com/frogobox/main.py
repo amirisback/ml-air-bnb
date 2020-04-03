f52cfd0514608db26e318499840f995660a29eab
amirisback <faisalamircs@gmail.com>
2020-04-04 01:45:40 +0700

    add: Trying K-Means

--- com/frogobox/main.py
+++ com/frogobox/main.py
@@ -17,5 +17,56 @@
 
 
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler
 
 data = pd.read_csv("raw/air_bnb.csv", delimiter=',')
+
+# --- Menghilangkan Kolom Yang Tidak Perlu ---
+# simpleData = data.drop([id,name,host_id,host_name,neighbourhood_group,neighbourhood,latitude,longitude,room_type,price,minimum_nights,number_of_reviews,last_review,reviews_per_month,calculated_host_listings_count,availability_365])
+data = data.drop(
+    ["id", "host_id", "host_name", "neighbourhood_group", "neighbourhood", "latitude", "longitude", "room_type",
+     "minimum_nights", "number_of_reviews", "last_review", "reviews_per_month", "calculated_host_listings_count",
+     "availability_365"]
+)
+data.head()
+
+# -- Menentukan variabel yang akan di klusterkan ---
+data_x = data.iloc[:, 1:3]
+data_x.head()
+
+# --- Memvisualkan persebaran data ---
+plt.scatter(data.name, data.price, s=10, c="c", marker="o", alpha=1)
+plt.show()
+
+# --- Mengubah Variabel Data Frame Menjadi Array ---
+x_array = np.array(data_x)
+print(x_array)
+
+# --- Menstandarkan Ukuran Variabel ---
+scaler = MinMaxScaler()
+x_scaled = scaler.fit_transform(x_array)
+x_scaled
+
+# --- Menentukan dan mengkonfigurasi fungsi kmeans ---
+kmeans = KMeans(n_clusters=3, random_state=123)
+# --- Menentukan kluster dari data ---
+kmeans.fit(x_scaled)
+
+# --- Menampilkan pusat cluster ---
+print(kmeans.cluster_centers_)
+
+# --- Menampilkan Hasil Kluster ---
+print(kmeans.labels_)
+# --- Menambahkan Kolom "kluster" Dalam Data Frame Driver ---
+data["kluster"] = kmeans.labels_
+
+# --- Memvisualkan hasil kluster ---
+output = plt.scatter(x_scaled[:, 0], x_scaled[:, 1], s=100, c=data.kluster, marker="o", alpha=1, )
+centers = kmeans.cluster_centers_
+plt.scatter(centers[:, 0], centers[:, 1], c='red', s=200, alpha=1, marker="s")
+plt.title("Hasil Klustering K-Means")
+plt.colorbar(output)
+plt.show()
