d2080454a136034b2fce80a1fd44be768f60e953
amirisback <faisalamircs@gmail.com>
2020-04-08 18:54:55 +0700

    update: refactoring code

--- com/frogobox/algorithm/classifiation.py
+++ com/frogobox/algorithm/classifiation.py
@@ -24,37 +24,17 @@ from com.frogobox.algorithm.evaluation import evaluation
 from com.frogobox.base.config import *
 
 
-def classification(path_data_result_clustering):
-    dataset = pd.read_csv(path_data_result_clustering)
-    # bagi data set kedalam nilai dan label
-    x_dataset = dataset.iloc[:, :-1].values
-    y_dataset = dataset.iloc[:, 4].values
-
-    x_train, x_test, y_train, y_test = train_test_split(x_dataset, y_dataset, test_size=0.30)
-
-    # Feature Scaling
-    scaler = StandardScaler()
-    scaler.fit(x_train)
-
-    x_train = scaler.transform(x_train)
-    temp_test = x_test
-    x_test = scaler.transform(x_test)
-
-    # Melatih dan Prekdiksi
-    classifier = KNeighborsClassifier(n_neighbors=2)
-    classifier.fit(x_train, y_train)
-    y_prediction = classifier.predict(x_test)
-
+def create_result_classification(data_test, y_prediction):
     item_price = []
     item_minimum_nights = []
     item_number_of_reviews = []
     item_availability_365 = []
 
-    for features in temp_test:
-        item_price.append(features[0])
-        item_minimum_nights.append(features[1])
-        item_number_of_reviews.append(features[2])
-        item_availability_365.append(features[3])
+    for column in data_test:
+        item_price.append(column[0])
+        item_minimum_nights.append(column[1])
+        item_number_of_reviews.append(column[2])
+        item_availability_365.append(column[3])
 
     data_frame = pd.DataFrame(
         {DATA_SET_PRICE: item_price, DATA_SET_MINIMUM_NIGHTS: item_minimum_nights,
@@ -62,5 +42,29 @@ def classification(path_data_result_clustering):
          DATA_SET_AVAILABILITY_365: item_availability_365, DATA_SET_CLASS: y_prediction})
 
     data_frame.to_csv(FILE_NAME_RESULT_CLASSIFICATION, index=False)
+
+
+def classification(path_data_result_clustering):
+    dataset = pd.read_csv(path_data_result_clustering)
+    # Labeling data dalam beberapa set
+    x_dataset = dataset.iloc[:, :-1].values
+    y_dataset = dataset.iloc[:, 4].values
+
+    x_training, x_test, y_training, y_test = train_test_split(x_dataset, y_dataset, test_size=0.30)
+
+    # Feature Scaling
+    scaler = StandardScaler()
+    scaler.fit(x_training)
+
+    x_training = scaler.transform(x_training)
+    temp_test = x_test
+    x_test = scaler.transform(x_test)
+
+    # Melatih dan Prekdiksi
+    classifier = KNeighborsClassifier(n_neighbors=2)
+    classifier.fit(x_training, y_training)
+    y_prediction = classifier.predict(x_test)
+
+    create_result_classification(temp_test, y_prediction)
 
     evaluation(y_test, y_prediction)
--- com/frogobox/algorithm/clustering.py
+++ com/frogobox/algorithm/clustering.py
@@ -21,14 +21,7 @@ from com.frogobox.base.config import FILE_NAME_RESULT_CLUSTERING
 from com.frogobox.algorithm.kmeans import Kmeans
 
 
-def clustering(path_file_raw_dataset):
-    fetch_raw_data = pd.read_csv(path_file_raw_dataset)
-    sorted_raw_data = fetch_raw_data[
-        [DATA_SET_PRICE, DATA_SET_MINIMUM_NIGHTS, DATA_SET_NUMBER_OF_REVIEWS, DATA_SET_AVAILABILITY_365]]
-    numpy_array = sorted_raw_data.values  # kembalikan numpy array
-    kmeans = Kmeans(2)
-    kmeans.fit(numpy_array)
-
+def create_result_clustering(kmeans):
     item_price = []
     item_minimum_nights = []
     item_number_of_reviews = []
@@ -36,15 +29,27 @@ def clustering(path_file_raw_dataset):
     labels = []
 
     for classification in kmeans.classes:
-        for features in kmeans.classes[classification]:
-            item_price.append(features[0])
-            item_minimum_nights.append(features[1])
-            item_number_of_reviews.append(features[2])
-            item_availability_365.append(features[3])
+        for column in kmeans.classes[classification]:
+            item_price.append(column[0])
+            item_minimum_nights.append(column[1])
+            item_number_of_reviews.append(column[2])
+            item_availability_365.append(column[3])
             label = classification + 1
             labels.append(DATA_SET_CLASS + "_" + str(label))
 
     data_frame = pd.DataFrame({DATA_SET_PRICE: item_price, DATA_SET_MINIMUM_NIGHTS: item_minimum_nights,
                                DATA_SET_NUMBER_OF_REVIEWS: item_number_of_reviews,
                                DATA_SET_AVAILABILITY_365: item_availability_365, DATA_SET_CLASS: labels})
+
+    # Create clustering result csv
     data_frame.to_csv(FILE_NAME_RESULT_CLUSTERING, index=False)
+
+
+def clustering(path_file_raw_dataset):
+    fetch_raw_data = pd.read_csv(path_file_raw_dataset)
+    sorted_raw_data = fetch_raw_data[
+        [DATA_SET_PRICE, DATA_SET_MINIMUM_NIGHTS, DATA_SET_NUMBER_OF_REVIEWS, DATA_SET_AVAILABILITY_365]]
+    numpy_array = sorted_raw_data.values  # kembalikan numpy array
+    kmeans = Kmeans(2)
+    kmeans.fit(numpy_array)
+    create_result_clustering(kmeans)
