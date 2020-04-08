ee2ecd3a096d43a3e40d744c923d3b79edfde1a8
amirisback <faisalamircs@gmail.com>
2020-04-08 16:32:53 +0700

    update: variable

--- com/frogobox/main.py
+++ com/frogobox/main.py
@@ -28,30 +28,34 @@ from com.frogobox.kmeans import Kmeans
 
 def main():
     fetch_raw_data = pd.read_csv(FILE_NAME_RAW_DATA_SET)
-    df = fetch_raw_data[[DATA_SET_PRICE, DATA_SET_MINIMUM_NIGHTS, DATA_SET_NUMBER_OF_REVIEWS, DATA_SET_AVAILABILITY_365]]
+    df = fetch_raw_data[
+        [DATA_SET_PRICE, DATA_SET_MINIMUM_NIGHTS, DATA_SET_NUMBER_OF_REVIEWS, DATA_SET_AVAILABILITY_365]]
     dataset = df.astype(float).values.tolist()
     X = df.values  # kembalikan numpy array
     km = Kmeans(2)
     km.fit(X)
-    prices = []
-    minimum_nightss = []
-    number_of_reviewss = []
-    availability_365s = []
+    item_price = []
+    item_minimum_nights = []
+    item_number_of_reviews = []
+    item_availability_365 = []
     labels = []
     for classification in km.classes:
         for features in km.classes[classification]:
-            price = features[0]
-            minimum_nights = features[1]
-            number_of_reviews = features[2]
-            availability_365 = features[3]
-            prices.append(price)
-            minimum_nightss.append(minimum_nights)
-            number_of_reviewss.append(number_of_reviews)
-            availability_365s.append(availability_365)
+            fetch_price = features[0]
+            fetch_minimum_nights = features[1]
+            fetch_number_of_reviews = features[2]
+            fetch_availability_365 = features[3]
+
+            item_price.append(fetch_price)
+            item_minimum_nights.append(fetch_minimum_nights)
+            item_number_of_reviews.append(fetch_number_of_reviews)
+            item_availability_365.append(fetch_availability_365)
             label = classification + 1
             labels.append(DATA_SET_CLASS + "_" + str(label))
-    df = pd.DataFrame({DATA_SET_PRICE: prices, DATA_SET_MINIMUM_NIGHTS: minimum_nightss, DATA_SET_NUMBER_OF_REVIEWS: number_of_reviewss,
-                       DATA_SET_AVAILABILITY_365: availability_365, DATA_SET_CLASS: labels})
+
+    df = pd.DataFrame({DATA_SET_PRICE: item_price, DATA_SET_MINIMUM_NIGHTS: item_minimum_nights,
+                       DATA_SET_NUMBER_OF_REVIEWS: item_number_of_reviews,
+                       DATA_SET_AVAILABILITY_365: fetch_availability_365, DATA_SET_CLASS: labels})
     df.to_csv(FILE_NAME_RESULT_CLUSTERING, index=False)
 
     dataset = pd.read_csv(FILE_NAME_RESULT_CLUSTERING)
@@ -72,22 +76,25 @@ def main():
     classifier = KNeighborsClassifier(n_neighbors=2)
     classifier.fit(X_train, y_train)
     y_pred = classifier.predict(X_test)
-    prices = []
-    minimum_nightss = []
-    number_of_reviewss = []
-    availability_365s = []
+    item_price = []
+    item_minimum_nights = []
+    item_number_of_reviews = []
+    item_availability_365 = []
     for features in temp_test:
-        price = features[0]
-        minimum_nights = features[1]
-        number_of_reviews = features[2]
-        availability_365 = features[3]
-        prices.append(price)
-        minimum_nightss.append(minimum_nights)
-        number_of_reviewss.append(number_of_reviews)
-        availability_365s.append(availability_365)
+        fetch_price = features[0]
+        fetch_minimum_nights = features[1]
+        fetch_number_of_reviews = features[2]
+        fetch_availability_365 = features[3]
+
+        item_price.append(fetch_price)
+        item_minimum_nights.append(fetch_minimum_nights)
+        item_number_of_reviews.append(fetch_number_of_reviews)
+        item_availability_365.append(fetch_availability_365)
+
     df = pd.DataFrame(
-        {DATA_SET_PRICE: prices, DATA_SET_MINIMUM_NIGHTS: minimum_nightss, DATA_SET_NUMBER_OF_REVIEWS: number_of_reviewss,
-         DATA_SET_AVAILABILITY_365: availability_365, DATA_SET_CLASS: y_pred})
+        {DATA_SET_PRICE: item_price, DATA_SET_MINIMUM_NIGHTS: item_minimum_nights,
+         DATA_SET_NUMBER_OF_REVIEWS: item_number_of_reviews,
+         DATA_SET_AVAILABILITY_365: fetch_availability_365, DATA_SET_CLASS: y_pred})
     df.to_csv(FILE_NAME_RESULT_CLASSIFICATION, index=False)
 
     # evaluasi
